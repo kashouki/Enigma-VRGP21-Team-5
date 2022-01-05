83743df5943cc07a8d5cfbc4ffad1a68c8d0e983
Wackye <j4170149@gmail.com>
2022-01-05 23:22:21 +0800

    pour and grab

--- Assets/detectPour.cs
+++ Assets/detectPour.cs
@@ -5,17 +5,36 @@ using UnityEngine;
 public class detectPour : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Transform pot;
     void Start()
     {
-        
+        GetComponent<ParticleSystem>().Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.up.y < -0.3f)
+        //Debug.Log("Pour: " + Vector3.Dot(transform.forward, new Vector3(0, -1, 0)));
+        Debug.Log(Mathf.Pow(pot.position.x - transform.position.x, 2) + Mathf.Pow(pot.position.z - transform.position.z, 2));
+        if (Vector3.Dot(transform.forward, new Vector3(0, -1, 0)) > 0.3f && Mathf.Pow(pot.position.x - transform.position.x, 2) + Mathf.Pow(pot.position.z - transform.position.z, 2) < 0.2f)
+        {
+            Debug.Log("above pot & pour");
+            GetComponent<ParticleSystem>().Play();
+        }
+        else
+        {
+            GetComponent<ParticleSystem>().Stop();
+        }
+        /*
+        if (Vector3.Dot(transform.forward, new Vector3(0, -1, 0)) > 0.3f)
         {
             Debug.Log("POUR");
+            GetComponent<ParticleSystem>().Play();
         }
+        else
+        {
+            GetComponent<ParticleSystem>().Stop();
+        }
+        */
     }
 }
